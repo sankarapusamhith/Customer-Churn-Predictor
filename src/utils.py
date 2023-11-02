f557e51cf6cc7ee4229b564a57258fdb0c9a52e4
Sankarapu Samhith <samhiths17@gmail.com>
2023-11-03 04:01:57 +0530

    deployment bug fix

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from keras.models import load_model
 
 
 def save_object(file_path, obj):
@@ -20,7 +21,7 @@ def save_object(file_path, obj):
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return load_model(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
